ab42ec505e83b2dd1c61753819dd82e9955fd007
MiracleAce1 <miracleekharo@gmail.com>
2023-02-18 15:48:09 -0800

    ace

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * return: 0(success)
+ */
+int main(void)
+{
+	char Ace;
+
+	for (Ace = 'a'; Ace <= 'z'; Ace++)
+	{
+		putchar(Ace);
+	}
+	putchar('\n');
+	return (0);
+}
