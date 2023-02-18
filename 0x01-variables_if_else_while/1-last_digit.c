ced62e152593d5d69dcdc54517cef602cd06919b
MiracleAce1 <miracleekharo@gmail.com>
2023-02-18 15:06:22 -0800

    ace

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,14 +5,13 @@
  * main -Entry point
  * Ace
  * return: 0 (success)
-*/
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n % 10 > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	else if (n % 10 == 0)
