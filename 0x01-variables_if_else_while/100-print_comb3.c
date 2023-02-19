40b218f91282afd15b566ddd1461dd5c7e26bd47
MiracleAce1 <miracleekharo@gmail.com>
2023-02-19 06:10:29 -0800

    ace

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*Ace  main entry*/
+int main(void)
+{
+	int a, b;
+
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 49; b <= 57; b++)
+		{
+			if (a != b && b > a)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
