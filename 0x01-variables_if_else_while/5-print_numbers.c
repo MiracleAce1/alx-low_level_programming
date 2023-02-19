79d29fe71b87704a657e9b140769b5e09c0441ab
MiracleAce1 <miracleekharo@gmail.com>
2023-02-18 17:03:53 -0800

    ace

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* Ace main entry point*/
+int main(void)
+{
+	int ace;
+
+	for (ace = 0; ace < 10; ace++)
+	{
+		printf("%d", ace);
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*Ace code entries */
+int main(void)
+{
+	int ace;
+
+	for (ace = 0; ace < 10; ace++)
+		Putchar(ace);
+	putchar('\n');
+	return (0);
+}
