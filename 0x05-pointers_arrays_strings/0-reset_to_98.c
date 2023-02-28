7847c07c5740e33d2b5d26146723606f89fb57f2
MiracleAce1 <miracleekharo@gmail.com>
2023-02-28 10:34:36 -0800

    ace

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <main.h>
+/* ace reset_to_98 */
+/* return: 0 success */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <main.h>
+/* ace swap_int*/
+/* return 0 success*/
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
