63be9224b8669f4b727614db14c751d216675763
MiracleAce1 <miracleekharo@gmail.com>
2023-03-30 18:14:20 -0700

    ace

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <main.h>
-/* ace reset_to_98 */
-/* return: 0 success */
+/* reset_to_98 - update the var using pointers */
+/* @n: init to update*/
+/* return: 0  */
 void reset_to_98(int *n)
 {
 	*n = 98;
