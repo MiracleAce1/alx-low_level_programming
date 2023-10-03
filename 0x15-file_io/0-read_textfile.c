8d9507fdcdf37358e3858132f6b080cbe126d8c4
MiracleAce1 <miracleekharo@gmail.com>
2023-10-02 18:48:32 -0700

    ace

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,28 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * read_textfile - Reads a textfile and prints the contents to the POSIX STDOUT
- * @filename: The name of the file to read from
- * @letters: The number of characters it should print to the STDOUT
- * Return: Returns the number of characters printed
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fild = 0, ace = 0;
-	char *buffer;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (!filename || !letters)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	fild = open(filename, O_RDONLY);
-	if (fild < 0)
-		return (0);
-
-	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
-		return (0);
-
-	ace = read(fild, buffer, letters);
-	ace = write(STDOUT_FILENO, buffer, chk);
-	if (ace < 0)
-		return (0);
-
-	close(fild);
-	free(buffer);
-	return (ace);
+	free(buf);
+	close(fd);
+	return (w);
 }
